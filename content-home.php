ef88f3c649d580ba9f9c30f688e0bb3cee65aa3f
Sam Vipond <sammy.potato@gmail.com>
2016-10-06 21:20:07 -0400

    update styles

--- content-home.php
+++ content-home.php
@@ -9,9 +9,4 @@
 	<li>Choral direction and composition, vocal arrangement</li>
 	<li>Workshops and private lessons in voice</li>
 </ul>
-<hr>
-<h3>L’ÉCOUTE PROFONDE</h3>
-<p class="lead">L’écoute profonde est une philosophie et une méthode pratique mise au point par la compositrice avant-gardiste Pauline Oliveros, qui permet de cultiver une meilleure appréciation du son et d’accroître les possibilités de connexions interactives entre la personne et son environnement, la technologie et les autres, en musique, dans les arts et dans la vie quotidienne.</p>
-<p class="lead">Contrairement à l’audition, l’écoute est un processus mystérieux qui demeure en partie inexpliqué. L’écoute repose sur l’accumulation d’expériences et d’associations, les neurones agissant en synchronie au sein de schémas établis dans le corps au long de toute une vie passée à percevoir des ondes sonores par les oreilles et la surface de la peau. L’écoute inclusive ouvre l’esprit à l’ensemble des mémoires, permettant de nouvelles associations. L’écoute exclusive, ou concentrée, identifie les détails. Les deux sont impliquées dans l’écoute profonde.</p>
-<p class="lead">Les exercices proviennent de plusieurs sources, incluant des pièces ou partitions composées à cette fin par Pauline Oliveros et d’autres praticiens de l’écoute profonde. On y retrouve le travail d’énergie, le travail du corps, des exercices de respiration, des vocalises, la méditation d’écoute et le travail sur le rêve.</p>
-<p class="lead">« Ces exercices ont pour but de calmer l’esprit et d’éveiller la conscience du corps et de sa circulation d’énergie, et de favoriser une attitude plus réceptive devant l’ensemble du continuum spatio-temporel du son. Cette réceptivité est essentielle à l’activité créatrice dans les arts et peut s’appliquer à toute discipline [dans les arts, ou dans la vie]. »</p>
+
